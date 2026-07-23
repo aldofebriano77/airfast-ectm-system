4ceba8f42c10fb6e05a1e6b2e3c0edfa97d6009e
Aldo Febriano <aldofebriano77@gmail.com>
2026-07-23 10:13:07 +0700

    menambahkan icon baru ke web

--- testing.py
+++ testing.py
@@ -39,7 +39,7 @@ except ImportError:
 # ======================================================================================
 st.set_page_config(
     page_title="PT. AIRFAST Indonesia ECTM Dashboard",
-    page_icon="images.png",  # Ganti dengan path file logo kamu
+    page_icon="airfasticon.png",  # Ganti dengan path file logo kamu
     layout="wide",
 )
 
